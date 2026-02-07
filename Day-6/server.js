912d4999954a55ca00e5de52743812cd795e4e89
spideyycode6 <spideyycode6@email.com>
2026-02-07 16:55:37 +0530

    Day-7

--- Day-6/server.js
+++ Day-6/server.js
@@ -1,7 +1,7 @@
 const app = require("./scr/app.js");
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://spideyycode_db_user:F0DaJbCuZ2xJC5Y0@cluster0.paeynny.mongodb.net/Day-6')
+mongoose.connect('nothing to connect')
 .then(()=>{
     console.log('database connected!!!');
 })
